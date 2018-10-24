6f627795f0ee42ca9f1dc4f9c0d3c029a4c4fa06
Jordan Brown <ampzgunz@hotmail.com>
2018-10-24 10:14:42 -0400

    Updated code to be more DRY
    Changed a few things, and optimized a few codes.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-function VendingMachine(){
-    this.loanAmount = 0;
-    this.funds = 10;
-    this.useLoan = false;
-    this.maintenance = false;
-    this.mathVisual = true;
+class VendingMachine{
+    constructor(){
+        this.loanAmount = 0, /* Loan amount is actually assigned at bottom of code */
+        this.funds = 10, /* You're starting balance */
+        this.useLoan = false, /* If this is set to true. Purchases will be deducted from your Loan rather than your balance*/
+        this.maintenance = false,  /* When set to true, this will cause a maintenance screen to appear */
+        this.mathVisual = true; /* When this is set to true, a visual example of each purchase will be presented. */
+    }
 }
 
+/* List of items available in our vending machine */
 let options = [
       {
           "coke": {
@@ -58,14 +61,14 @@ let options = [
               "category": "chips"
           },
           "reese": {
-              "productName": "Doritos",
+              "productName": "Reese",
               "productPrice": 3.15,
               "productDesc": "Feast your mouth on this",
               "productCode": "a107",
               "category": "chocolate"
           },
           "snickers": {
-              "productName": "Jelly Beans",
+              "productName": "Snickers",
               "productPrice": 3.15,
               "productDesc": "Your not yourself when your hungry",
               "productCode": "a108",
@@ -95,11 +98,13 @@ let options = [
     }
 ];
 
+/* Function to store variables related to the user */
 VendingMachine.prototype.user = function(){
     console.log(`Money: $${this.funds}`);
     this.loanInfo();
 }
 
+/* Information about ongoing loans are displayed with this func */
 VendingMachine.prototype.loanInfo = function(){
     if(this.loanAmount <= 0){
         console.log(`Loan Status: %cNo Pending Loan.`,`color:green`);
@@ -118,13 +123,17 @@ VendingMachine.prototype.takeLoan = function(amount){
 }
 
 VendingMachine.prototype.makePurchase = function(code,price){
+
     if(this.maintenance){
         return true;
     } else{
+
       let successMsg = 'color:green;font-weight:bold;',
           errorMsg = 'color:red;font-weight:bold;';
+
       for(let y = 0; y < options.length; y++){
           let obj = options[y];
+
           for(let item in obj){
               if(obj.hasOwnProperty(item)){
                   let getCode = obj[item];
@@ -135,13 +144,16 @@ VendingMachine.prototype.makePurchase = function(code,price){
                               console.log(`Current Balance: $${this.funds}`);
                               console.log(`Item Price: $${getCode.productPrice}`);
                               console.log(`Calculation: ${this.funds} - ${getCode.productPrice} =`, (this.funds - getCode.productPrice));
+
                               if(this.mathVisual){
+
                                 function visualization(balance,cost){
                                     let drawing;
                                     let fullCost;
                                     let lines = 7;
                                     let finalSum;
                                     let sym;
+
                                     for(var j = 0; j<balance+1; j++){
                                         drawing = j;
                                         for(var y = 0; y<Math.round(getCode.productPrice+1); y++){
@@ -155,6 +167,7 @@ VendingMachine.prototype.makePurchase = function(code,price){
                                             finalSum = " "+drawing-fullCost;
                                         }
                                     }
+
                                     console.log(`\n%cFormula Visualization`,`padding:0.2%;color:blue;font-size:14px;font-size:bold;text-decoration:underline;`);
                                     console.log(`%c${drawing}\n-\n${fullCost}\n${sym}\n${finalSum}\n\n`,`font-style:italic;;color:blue;font-size:14px;`);
                                 }
@@ -191,7 +204,7 @@ VendingMachine.prototype.makePurchase = function(code,price){
 VendingMachine.prototype.outOfOrder = function(msg){
     if(this.maintenance){
         console.log(``);
-        msg = message = `This vending machine is currently down for maintenance`;
+        msg = `This vending machine is currently down for maintenance`;
         console.log(`%c ${msg} !`,`color:red;font-size:18px;border:1px solid red;padding:1% 5% 1% 1%;font-family:Calibri;border-style:dotted;`);
         console.log(``);
     } else{
@@ -201,13 +214,19 @@ VendingMachine.prototype.outOfOrder = function(msg){
 
 VendingMachine.prototype.search = function(code){
     let newItem = [];
+
     for(let x = 0; x < options.length; x++){
         let myObj = options[x];
         for(var key in myObj){
             if(typeof Object){
                 let item = myObj[key];
-                if(item["productCode"] == code){
+                if(item["productCode"] == code || item["productCode"].toUpperCase() == code){
                     newItem.push(item);
+                }
+                else if(item.hasOwnProperty("category")){
+                    if(item.category==code){
+                        console.log(`\nInformation For Product By Category: %c${code}`, `color:green;font-weight:bold;text-transform:uppercase;`, `${JSON.stringify(item, undefined, 2)}`);
+                    }
                 }
             }
         }
@@ -219,27 +238,12 @@ VendingMachine.prototype.search = function(code){
     });
 }
 
-VendingMachine.prototype.byCategory = function(category){
-    let catInfo = [];
-    for(let x = 0; x < options.length; x++){
-        let obj = options[x];
-        for(let key in obj){
-            let item = obj[key];
-            if(item.hasOwnProperty("category")){
-                if(item.category==category){
-                    console.log(`\nInformation For Product By Category: %c${category}`, `color:green;font-weight:bold;text-transform:uppercase;`, `${JSON.stringify(item, undefined, 2)}`);
-                }
-            }
-        }
-    }
-}
-
 let vender = new VendingMachine();
 vender.outOfOrder();
 vender.user();
 vender.takeLoan(52);
 
-let searchByCode = vender.search("a100");
-let searchByCat = vender.byCategory("chocolate");
+vender.search("chocolate");
+vender.search("A101");
+
 vender.makePurchase("a100", 2.55); 
-vender.makePurchase("a101", 1.25);
